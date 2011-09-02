67ed75030c78bb898e0af2c07497c16f249e40eb
Steve Losh <steve@stevelosh.com>
2011-09-02 09:52:05 -0400

    Add threesome_leader config option.

--- autoload/threesomelib/util/keys.py
+++ autoload/threesomelib/util/keys.py
@@ -1,20 +1,33 @@
 import vim
 from bufferlib import buffers
+from ..settings import setting
 
 
-def bind(key, to, options='', mode=None, leader='<localleader>'):
+def bind(key, to, options='', mode=None, leader=None):
+    if not leader:
+        leader = setting('leader', '<localleader>')
+
     vim.command('nnoremap %s %s%s %s' % (options, leader, key, to))
 
-def unbind(key, options='', leader='<localleader>'):
+def unbind(key, options='', leader=None):
+    if not leader:
+        leader = setting('leader', '<localleader>')
+
     vim.command('unmap %s %s%s' % (options, leader, key))
 
-def bind_for_all(key, to, options='', mode=None, leader='<localleader>'):
+def bind_for_all(key, to, options='', mode=None, leader=None):
+    if not leader:
+        leader = setting('leader', '<localleader>')
+
     with buffers.remain():
         for b in buffers.all:
             b.open()
             bind(key, to, options, mode, leader)
 
-def unbind_for_all(key, options='', leader='<localleader>'):
+def unbind_for_all(key, options='', leader=None):
+    if not leader:
+        leader = setting('leader', '<localleader>')
+
     with buffers.remain():
         for b in buffers.all:
             b.open()
